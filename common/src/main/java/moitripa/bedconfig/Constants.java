9b1eae1780f1d7029a1e226b7ea2387ac90284e9
moi-tripa <moitripa2@gmail.com>
2025-10-26 18:14:05 -0600

    Refactor

---
+++ common/src/main/java/moitripa/bedconfig/Constants.java
@@ -0,0 +1,11 @@
+package moitripa.bedconfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "bedconfig";
+	public static final String MOD_NAME = "Bed Config";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/moitripa/bedconfig/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package moitripa.bedconfig.platform;
+
+import moitripa.bedconfig.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
